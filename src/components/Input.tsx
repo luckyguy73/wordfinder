9a164454d29f7e1be6db5ed4be59d99b2a1a06b7
Ian De Bie <ian@debie.us>
2023-07-03 12:44:46 -0500

    add more space for input boxes

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -18,7 +18,7 @@ export default function Input({ id, label, required, onChange, onClick, value}: 
       </label>
       <div className='relative mr-2'>
         <input
-          className='pl-2 pr-9 rounded border border-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full'
+          className='pl-2 pr-9 py-1 rounded border border-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full'
           type='text'
           id={id}
           name={id}
