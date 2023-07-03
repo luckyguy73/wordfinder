0a9178d1b900fe4cb2667adb261d0f9f97bbadda
Ian De Bie <ian@debie.us>
2023-07-02 21:09:10 -0500

    import wordlist.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
+// import { queryDictionary } from '@lib/database';
 import Form from '@components/Form';
-import { queryDictionary } from '@lib/database';
+import { readFile } from 'fs/promises';
 
 export default async function Home() {
-  const { data, error }: { data: any; error: any } = await queryDictionary();
+  // using supabase
+  // const { data, error }: { data: any; error: any } = await queryDictionary();
+  const data = await readFile('./src/lib/wordlist.json', 'utf-8');
+  const words: { word: string }[] = JSON.parse(data);
+  console.log(words);
 
   return (
     <section className='mx-auto px-4 max-w-xl bg-white border-2 border-slate-200 rounded shadow-lg'>
       <div className='py-6 text-4xl bg-white'>
         <h1 className='text-center'>Enter Letters</h1>
       </div>
-      <Form words={data} />
+      <Form words={words} />
     </section>
   );
 }
