11a42e97e9698e3a122b2ccef95f81f8007c18fa
Ian De Bie <ian@debie.us>
2024-02-10 09:45:40 -0600

    update navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
 
   return (
     <section className='mx-auto px-4 max-w-xl bg-white border-2 border-slate-200 rounded shadow-lg'>
-      <div className='py-3 text-4xl bg-white'>
+      <div className='py-3 text-3xl bg-white'>
         <h1 className='text-center'>Enter Letters</h1>
       </div>
       <Form words={wordsArray} />
